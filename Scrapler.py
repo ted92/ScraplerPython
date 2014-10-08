f96a017df4b64395de3c4418601bb0f85c2a4117
Enrico <enrico.tedeschi1@gmail.com>
2014-10-09 01:55:33 +0200

    Update Scrapler.py

--- Scrapler.py
+++ Scrapler.py
@@ -4,28 +4,56 @@ Created on 06/ott/2014
 @author: Enrico Tedeschi
 '''
 import requests
+import pymysql  
 
+import sqlite3  #to create the database where to put all the data
+from bs4 import BeautifulSoup
 
-from bs4 import BeautifulSoup   #using BeautifulSoup4
-
-url = "http://www.paginegialle.it/pgol/4-idraulico/3-Milano%20%28MI%29"   #given url
+url = "http://www.paginegialle.it/pgol/4-idraulico/3-Milano%20%28MI%29"
 r = requests.get(url)
 
+#creating the db
+db = sqlite3.connect("PagineGialle.sqlite")
 
-soup = BeautifulSoup(r.content)   #object soup in soup variable
+cursor = db.cursor()
+cursor.execute("""CREATE TABLE IF NOT EXISTS PagineGialle
+     (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+     name TEXT NOT NULL,
+     address TEXT,
+     tel  TEXT)""" )    #create a new table only if it doesnt exists yet
+db.commit()
+
+
+#print r.content
+
+soup = BeautifulSoup(r.content)
+#print soup.prettify()
 
     
-g_data = soup.findAll("div", {"class": "item_sx"})    #here is where I have all the informations I need
+g_data = soup.findAll("div", {"class": "item_sx"}) #here is where I have all the informations I need
 
-name = range(10000)     #high random number, not relevant
+#high number range to avoid overflow, to improve with dynamic structure
+name = range(10000)
 address = range(10000)
+telephone = range(10000)
+
 i = 0
 for item in g_data:
-    #print item.contents[1].text
-    name[i] = item.find("h2", {"class":"rgs"}).text     #the names are in <h2 class="rgs">
-    
-    #the address is a composed string from two <span> HTML tags
+    name[i] = item.find("h2", {"class":"rgs"}).text
     address[i] = item.find("span", {"class":"street-address"}).text + " " + item.find("span", {"class":"locality"}).text
+    telephone[i] = item.find("div", {"class":"tel"}).text
+    
+    cursor.execute("""INSERT INTO PagineGialle (name, address, tel) VALUES
+                  (?, ?, ?)""", (name[i], address[i], telephone[i]) )
+    
+    db.commit()
+    
+    #just for test
     print name[i]
     print address[i]
+    print telephone[i]
     print"\n\n"
+    
+    
+    
+db.close()  #close the connection to the database
